3d454e51cfe067e339e9a099c5b1cbc2f7c50a50
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-01 11:16:39 -0300

    Criada chamada para API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmeService } from './services/filme.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private filmeService: FilmeService) {}
+
+  ngOnInit(): void {
+    this.filmeService.selecionarFilmesPopulares().subscribe((f) => {
+      console.log(f);
+    });
+  }
+}
