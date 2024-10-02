b982a7f161338fc6b42b0c21822ff49f7ea19104
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-02 12:10:51 -0300

    Adicionada navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ListagemFilmesComponent } from './components/listagem-filmes/listagem-f
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePtBr from '@angular/common/locales/pt';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ListagemFilmesComponent],
+  imports: [ListagemFilmesComponent, NavbarComponent],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
